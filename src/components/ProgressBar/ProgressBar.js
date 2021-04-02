b03eafe0050518c6b7cc23926bb6b60914be4c5e
SimonGPort <simon.giroux.portelance@gmail.com>
2021-04-01 22:30:28 -0400

    progressBar

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -5,8 +5,49 @@ import styled from 'styled-components';
 import { COLORS } from '../../constants';
 import VisuallyHidden from '../VisuallyHidden';
 
+const ProgressBarContainer=styled.div`
+width:370px;
+box-shadow: inset 0px 2px 4px rgba(128, 128, 128, 0.35);
+background: rgba(128, 128, 128, 0.15);
+
+
+
+border-radius: ${props =>{ 
+  if(props.size==="large"){
+    return '8px'
+  }else{return '4px'}}};
+
+height:${props =>{ 
+  if(props.size==="large"){
+    return '24px'
+  }
+  if(props.size==="medium"){
+    return '12px'
+  }
+  if(props.size==="small"){
+    return '8px'
+  }
+}
+}
+`
+
+const Progress=styled.div`
+background: #4747EB;
+border-radius: 4px 0px 0px 4px;
+height:100%;
+width:${props =>{return props.value+'%' }}
+`
+
+
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+
+
+
+
+
+  return (<ProgressBarContainer size={size}>
+<Progress value={value}/>
+  </ProgressBarContainer>)
 };
 
 export default ProgressBar;
