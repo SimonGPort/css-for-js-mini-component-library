ba1103d1936152dde5e9fbed08a866d88fc35cfc
SimonGPort <simon.giroux.portelance@gmail.com>
2021-04-03 22:09:31 -0400

    iconInput

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -6,6 +6,54 @@ import { COLORS } from '../../constants';
 import Icon from '../Icon';
 import VisuallyHidden from '../VisuallyHidden';
 
+let Container = styled.div`
+display:flex;
+align-items:center;
+width:${props => { return (props.width + "px") }};
+padding:4px;
+height:max-content;
+border-bottom:2px solid #000000;
+color: ${COLORS.gray700}
+&:hover {
+  color:black;
+}
+`
+let Logo=styled(Icon)`
+margin-right:${props=>{
+if(props.sizeInput==="small"){return ("15px")}
+if(props.sizeInput==="large")
+    {return ("25px")}
+}}
+`
+const Input=styled.input`
+
+width: ${props=>{
+  let logoSize=0
+  if(props.size==='small'){
+    logoSize=10.6+15
+  }
+  if(props.size==='large'){
+    logoSize=16+25
+  }
+let newWidth=props.width-logoSize
+return newWidth+"px"
+}
+};
+outline:none;
+border:none;
+height: ${
+  props=>{
+    if(props.size==='small'){
+      return "16px"
+    }
+    if(props.size==='small'){
+      return "21px"
+    }
+  }
+}
+`
+
+
 const IconInput = ({
   label,
   icon,
@@ -13,7 +61,25 @@ const IconInput = ({
   size,
   placeholder,
 }) => {
-  return 'TODO';
+
+
+let sizeLogo={
+  'small':10.6,
+  'large':16
+}
+
+
+
+  return <Container width={width} >
+    <Logo
+      id={icon}
+      strokeWidth={1}
+      size={sizeLogo[size]}
+      sizeInput={size}
+    />
+    <Input placeholder={placeholder} size={size} width={width}/>
+    
+    </Container >;
 };
 
 export default IconInput;
